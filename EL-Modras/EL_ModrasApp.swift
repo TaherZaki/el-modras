065ec5b65a5df847a7f4a5f7f2277a195a481916
Taher Zaki <tmohamed@nwc.com.sa>
2026-02-24 23:44:56 +0300

    Initial Commit

---
+++ EL-Modras/EL_ModrasApp.swift
@@ -0,0 +1,17 @@
+//
+//  EL_ModrasApp.swift
+//  EL-Modras
+//
+//  Created by Taher on 07/09/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct EL_ModrasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
